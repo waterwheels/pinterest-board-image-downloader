79fca291c49107c3d5e12aa6abb0a7ccf7330801
waterwheels <joe.lombard@me.com>
2023-04-14 15:26:47 +1000

    rewrote to use XPATH selectors
    currently it's not finding all the images on the page. Will try following along in an IDLE shell to inspect more detail, or running in a debugger

--- downloadPinterestBoardImages.py
+++ downloadPinterestBoardImages.py
@@ -5,59 +5,89 @@ import time
 
 from argparse import ArgumentParser
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-def downloadPinterestImages(link):
-    browser = webdriver.Firefox(executable_path=r'geckodriver.exe')
 
+
+#xpath_pinboard = "//div[@data-test-id='MobileFeed']" # This div contains the actual pinterest board, not the "more like this" section
+xpath_pinboard = "//div[contains(@id, 'boardfeed')]" # This div contains the actual pinterest board, not the "more like this" section
+xpath_img = "//div[@class='PinCard__imageWrapper']/descendant::img" # Filters out only pin imgs, not user avatars
+xpath_end = "//h2[text()='More like this']" # matches the end of the pin-board
+
+
+def downloadPinterestImages(link, max_scolls, sleep_delay):
+    opts = Options()
+    # opts.headless = True
+    browser = webdriver.Firefox(executable_path=r'geckodriver.exe', options=opts)
+
+    # Load page
     browser.get(link)
 
-    time.sleep(2)
-    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match=False
-    limit=7 #limit of scrolls
-    while(match==False and limit>0): #auto scroll till end or till limit
-        lastCount = lenOfPage
-        time.sleep(3)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        limit = limit-1;
-        if lastCount==lenOfPage:
-            print("stop scrolling")
-            match=True
-        else:
-            print("scrolling..")
+    # Scroll the page until we find the "more like this" heading, can't scroll more, or exceed max number of scrolls
+    initial_height = browser.execute_script("return document.body.scrollHeight")
 
-    response = browser.page_source#.encode(encoding='UTF-8')
+    c = 0
+    while(True):
+        c += 1
+        print(f"Last scroll height {initial_height}")
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_delay)
+        new_height = browser.execute_script("return document.body.scrollHeight")
 
-    toDel =[]
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
+        if new_height == initial_height:
+            print("Found end of page, no more page to scroll")
+            break
 
-    print(len(urls))
-    for i in range(len(urls)):
-        if(urls[i][-4:]==".jpg"):
-            urls[i]=re.sub('.com/.*?/','.com/originals/',urls[i],flags=re.DOTALL)
-        else:
-            urls[i]= ""
+        if c > max_scolls:
+            print(f"reached maximum number of scrolls: {max_scolls}")
+            break
 
-    urls = list(set(urls))
+        if len(browser.find_elements(By.XPATH, xpath_end)) > 0:
+            print("Found end of board")
+            break
 
-    urls = list(filter(None, urls)) # fastest
-    urls = list(filter(bool, urls)) # fastest
-    urls = list(filter(len, urls))  # a bit slower
+        initial_height = new_height
+    
+    time.sleep(10)
 
-    with open('raw.txt', 'w') as f:
-        for url in urls:
-            f.write("%s\n" % url)
-    f.close()
+    # Get the div containing the pins
+    block = browser.find_element(By.XPATH, xpath_pinboard)
 
-    for i in range(len(urls)):
+    # Grab only pin images (avoid users' profile pictures) 
+    img_elements = block.find_elements(By.XPATH, xpath_img)
+    test = block.find_elements(By.XPATH, "//img")
+
+    print(f"Found {len(img_elements)} elements matching {xpath_img}")
+    print(f"Found {len(img_elements)} image elements")
+
+    images = []
+    for this_image in img_elements:
+        smol = this_image.get_attribute('src')
+        original = re.sub('.com/.*?/','.com/originals/',smol,flags=re.DOTALL)
+        images.append([smol,original])
+    
+    for img in images:
+        name = img[0].rsplit('/', 1)[-1]
+        print("Downloading " + name)
         try:
-            urllib.request.urlretrieve(urls[i], "images/"+str(i)+"-image.jpg")
+            print("    URL "+img[1])
+            #urllib.request.urlretrieve(img[1], "images/"+name)
         except:
-            print("Broken Link") 
+            print("        Broken link to original, trying resampled image link")
+            try:
+                print("        URL "+img[1])
+                #urllib.request.urlretrieve(img[0], "images/thumb-"+name)
+            except:
+                print("       both links broken. Sorry :(")
+
+    #browser.quit()
         
         
 parser = ArgumentParser()
 parser.add_argument("pinterest_URL")
+parser.add_argument('-s', '--scroll_limit', type=int, default=10)
+parser.add_argument('-d', '--delay', type=int, default=3)
 args = parser.parse_args()
 
-downloadPinterestImages(args.pinterest_URL)
+downloadPinterestImages(args.pinterest_URL, args.scroll_limit, args.delay)
